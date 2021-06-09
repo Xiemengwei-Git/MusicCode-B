0be5f50070812b4cbe54d6d7d9325a558990d119
xie <1538905459@qq.com>
2021-06-09 22:43:28 +0800

    返回工具类

---
+++ src/main/java/com/MusicCode/backEnd/untils/R.java
@@ -0,0 +1,19 @@
+package com.MusicCode.backEnd.untils;
+
+import lombok.Data;
+
+/**
+ * 返回对象
+ */
+
+@Data
+public class R {
+    // 状态码
+    private Integer code;
+    // 数据
+    private Object data;
+    // 信息
+    private String msg;
+    // 记录数
+    private Integer total;
+}
